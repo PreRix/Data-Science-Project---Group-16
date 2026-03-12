1a3d7a9f75400503334a8f181ccf6a1705fdd647
anthimewillmann <anthime.willmann@gmail.com>
2026-03-12 23:58:18 +0100

    Change filter condition for Zst column in load_data

--- Code/Website/rq8.py
+++ Code/Website/rq8.py
@@ -12,7 +12,7 @@ st.title("Rush-Hour Traffic & Home Office Effect (2021–2025)")
 def load_data():
     df = (
         pl.read_csv(os.path.join(BASE_DIR, "MergeData", "holy_file.csv"), infer_schema_length=0)
-        .filter(pl.col("Zst") == "1104")
+        .filter(pl.col("Zst") == "1194")
         .with_columns(
             pl.col("date").str.to_datetime("%d.%m.%Y %H:%M").alias("datetime")
         )
