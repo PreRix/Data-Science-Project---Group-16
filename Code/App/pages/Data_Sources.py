50065262a46f48843d65daafcfe1c4109ccd35c6
anthimewillmann <anthime.willmann@gmail.com>
2026-03-18 14:51:07 +0100

    Document data sources and descriptions in Data_Sources.py
    Added detailed descriptions and sources for various data used in the project, including vehicle counting, registration counts, weather, air quality, and Kieler Woche data.

--- Code/App/pages/Data_Sources.py
+++ Code/App/pages/Data_Sources.py
@@ -1,1 +1,53 @@
+st.title("The Data we used")
+st.write("Here you can find all raw data we used for this project. Expand on the data you want to know more about.")
 
+with st.expander("Vehicle Counting Data"):
+    st.markdown("""
+        For information about the vehicle count on the streets in and around Kiel we used the data from BASt, the "Bundesanstalt für Straßen- und Verkehrswesen". 
+        The data is provided as zip-archives per year for Autobahnen and Bundesstraßen; each of which contains folders with 12 raw-data files and one corresponding meta-data file explaining the data. 
+        The data is organized in .csv-files and is sampled per hour.  
+        Furthermore the vehicle count is given per direction of travel and info about the counted vehicle type is given.
+        
+
+        The BASt is the national research center for transportaion and traffic in Germany. Therefore we declare the data as reliable.
+        For understanding the datasets a data description in form of a PDF file is provded on the BASt website.
+    """)
+  
+    st.image("images/BASt_example.png", caption = "Sample for BASt data structure", width = "stretch")
+    st.info("**Source:** https://www.bast.de/DE/Publikationen/Daten/Verkehrstechnik/DZ-Richtung.html?nn=427910")
+
+with st.expander("Vehicle Registration Counts"):
+    st.markdown("""
+        To retrive information about the vehicle registration counts for Kiel, we took the yearly registration reports from the "Kraftfahrt Bundesamt" (KBA). 
+        These are provided in Excel files containing the count of all registered vehicles in Germany. The counts are also differentiated into engine types.
+
+        The KBA is a German federal agency under the jurisdiction of the Ministry for Digtial and Transport. Therefore the data is trustworthy and of quality.
+    """)
+    st.info("**Source:** https://www.kba.de/DE/Statistik/Produktkatalog/produkte/Fahrzeuge/fz1_b_uebersicht.html?nn=835828")
+
+with st.expander("Weather & Air Quality Data"):
+    st.markdown("""
+        For collecting historical weather and air quality data we used the APIs from Open-Meteo.com. 
+        
+        #### Weather API: 
+        The Weather API holds the data in hourly and daily sampling. The data is based on reanalysis datasets and combines weather station, aircraft, buoy, radar and satellite observations to create a
+        comprehensive record of past weather conditions.
+        The spatial resolution is varying between 5 - 10km; therefore the values are not 100% accurate in terms of an explicit location.
+        From all selectable data (over 30 variables) we only selected the ones we needed for our research.
+
+        #### Air Quality API:
+        The Air Quality API holds the data in hourly sampling. The data is based on reanalysis datasets and forecasts from the "Copernicus Atmosphere Monitoring Service" (CAMS) providing a spatial 
+        resolution between 11 - 45km. As for the weather data; the values can not be seen as 100% accurate in terms of an explicit location.
+        Here we selected only the variables of interest as well, and not all the API is offering.
+
+        Both APIs provide an interactive web interface for trying the API out. 
+    """)
+    st.info("**Source Weather data:** https://open-meteo.com/en/docs/historical-weather-api")
+    st.info("**Source Air Quality data:** https://open-meteo.com/en/docs/air-quality-api")
+
+with st.expander("Kieler Woche Data"):
+    st.markdown("""
+        The Kieler Woche data we wanted to retrive was mainly the dates, from when to when the Kieler Woche took place each year. As there is no dataset or API holding this information, 
+        we collected the data ourselves from the official Kieler Woche website, together with the provided estimated visitor counts for each year and wrote them into a .csv-file.
+    """)
+    st.info("**Source:** https://www.kieler-woche.de/de/medien/meldung.php")
